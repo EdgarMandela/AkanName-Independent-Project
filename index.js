04b8c34cc24654d907dddaafb7cbcbffa0f7fd08
EdgarMandela <mandela0edgar@gmail.com>
2019-12-02 05:26:38 +0300

    added function for validating month to the js

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ function myFunction() {
     if ((date <= 0) || (date > 31)) {
       alert("invalid date ! please enter number between 1 - 31");
     }
+}
+var month = document.getElementById("month").value;
+if ((month <= 0) || (month > 12)) {
+  alert("invalid Month ! please enter number between 1 - 12");
+}
+}
+
   
